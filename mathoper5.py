1ab44ce188aeba02410193fb4c6c735d3507e772
ccortes1994 <44816373+ccortes1994@users.noreply.github.com>
2018-11-21 12:39:35 -0700

    Add files via upload

---
+++ mathoper5.py
@@ -0,0 +1,36 @@
+'''
+math operations
+'''
+
+a = 25
+b = 10
+c = 7
+
+print("Display the sum of :",a," + ",c," = ",a + c)
+print("Display the sum of :",a," + ",b," = ",a + b)
+
+# subtraction
+
+print("Display the difference of :",a," - ",c," = ",a - c)
+
+# multiplication *
+
+print("Display the product of :",a," * ",c," = ",a * c)
+
+# division
+
+print("Display the quotient of :",a," / ",c," = ",a / c)
+
+# integer division
+
+print("Display the quotient of integer division of :",a," // ",c," = ",a // c)
+
+# the remainder of an int dic, %
+
+print("Display the remainder f int div of :",a," % ",c," = ",a % c)
+print(a,"modulus" )
+
+
+# exponents
+
+ print(3**5)
