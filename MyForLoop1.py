dd2be48955d9bcda883d5a4259f611e75a2d1f44
ccortes1994 <44816373+ccortes1994@users.noreply.github.com>
2019-01-08 11:59:07 -0700

    Add files via upload

---
+++ MyForLoop1.py
@@ -0,0 +1,18 @@
+#print()
+
+#for i in range(1,10):
+#    print(i)
+
+#for j in range(1,10):
+#   print(j,end='')
+#print()
+#for k in range(1,10):
+#    print(k, ' ', end='')
+#print()
+#for l in range(0,1000,100):
+#   print('{0:5d}'.format(l))
+
+for row in range(1,10):
+    for col in range(1,5):
+        print(row,col,'  ',end='')
+    print()
